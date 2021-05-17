02ed2907f6e96bb9b52f4e9b3aca37c32f86bf57
Александр Перепадин <alyansalex@gmail.com>
2021-05-17 20:15:25 +0300

    First

---
+++ src/Main.java
@@ -0,0 +1,29 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int balance = -500;
+        int transfer = 1600;
+
+        int bonus;
+
+        if (transfer >= 1000) {
+            bonus = transfer / 100;
+        } else {
+            bonus = 0;
+        }
+        int total = balance + transfer + bonus;
+
+        System.out.println("Текущий баланс:");
+        System.out.println(balance);
+
+        System.out.println("Сумма перевода:");
+        System.out.println(transfer);
+
+        System.out.println("Всего начислено бонуснов:");
+        System.out.println(bonus);
+
+        System.out.println("Общий баланс:");
+        System.out.println(total);
+
+    }
+}
